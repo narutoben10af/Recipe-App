35d9a5e6b00c3984b1290fa23370c19f94af2f6a
narutoben10af <limsieian@me.com>
2021-10-22 09:41:51 +0900

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Lim Si Eian on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
